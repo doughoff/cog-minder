6868179297497156f68190c76fb2d9e6f57d29e0
noemica <noemica@protonmail.com>
2022-11-06 22:23:51 -0700

    Updates

--- simulator.0c4877d2.js
+++ simulator.0c4877d2.js
@@ -82285,7 +82285,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38625" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40229" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
